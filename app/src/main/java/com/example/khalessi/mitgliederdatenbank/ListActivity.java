c9e89bb1bec22f3728ee0446055b3ad2e4c4c7e5
alexasgithub <alexandra.filbert@googlemail.com>
2018-01-31 13:10:28 +0100

    ListView added

--- app/src/main/java/com/example/khalessi/mitgliederdatenbank/ListActivity.java
+++ app/src/main/java/com/example/khalessi/mitgliederdatenbank/ListActivity.java
@@ -5,12 +5,17 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListActivity extends AppCompatActivity {
-    SQLiteDatabase mitgliederDatenbank;
+    private SQLiteDatabase mitgliederDatenbank;
+
+    private int positionClicked = -1;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,7 @@ public class ListActivity extends AppCompatActivity {
         mitgliederDatenbank = moh.getWritableDatabase();
 
         String[] projection = {MitgliederOpenHelper.COL_NAME_ID, MitgliederOpenHelper.COL_NAME_NAME, MitgliederOpenHelper.COL_NAME_ANSCHRIFT};
-        Cursor cursor = mitgliederDatenbank.query(MitgliederOpenHelper.TABLE_NAME_MITGLIEDER, projection, "1=1", null, null, null, null);
+        cursor = mitgliederDatenbank.query(MitgliederOpenHelper.TABLE_NAME_MITGLIEDER, projection, "1=1", null, null, null, null);
    /*     cursor.moveToFirst();
 
         db_liste.setText("");
@@ -46,9 +51,27 @@ public class ListActivity extends AppCompatActivity {
         ListView db_liste = (ListView) findViewById(R.id.dbListe);
         db_liste.setAdapter(adapter);
         db_liste.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        db_liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(ListActivity.this, "Position: " + position, Toast.LENGTH_SHORT).show();
+                positionClicked = position;
+            }
+        });
     }
+
 
     public void onZurueckClick(View view) {
         finish();
+    }
+
+    public void auswahlLoeschenClick(View view) {
+        if (positionClicked != -1) {
+            cursor.moveToPosition(positionClicked);
+            int id = cursor.getInt(0);
+            mitgliederDatenbank.delete(MitgliederOpenHelper.TABLE_NAME_MITGLIEDER,
+                    MitgliederOpenHelper.COL_NAME_ID+" = "+id, null);
+        }
+        positionClicked=-1;
     }
 }
